e3caa219798611db98c4a64057a3bcf05b855046
anonymous <anonymous_watermark@outlook.com>
2025-09-17 21:30:14 +0800

    add audio

--- data/data.js
+++ data/data.js
@@ -1,5 +1,97 @@
-const data_list = [{
-    "dataset": "LibriSpeech",
-    "origin": "demo_audio/61-70968-0000_8455-210777-0012.wav",
-    "wmd": "demo_audio/61-70968-0000_8555-292519-0000.wav",
-}]
+const data_list = [
+// dataset1    
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_61-70970-0028.wav",
+    "wmd": "demo/librispeech/ours/librispeech_61-70970-0028_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_237-126133-0006.wav",
+    "wmd": "demo/librispeech/ours/librispeech_237-126133-0006_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_1089-134686-0034.wav",
+    "wmd": "demo/librispeech/ours/librispeech_1089-134686-0034_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_2094-142345-0054.wav",
+    "wmd": "demo/librispeech/ours/librispeech_2094-142345-0054_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_5105-28241-0004.wav",
+    "wmd": "demo/librispeech/ours/librispeech_5105-28241-0004_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_5105-28241-0015.wav",
+    "wmd": "demo/librispeech/ours/librispeech_5105-28241-0015_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_7127-75947-0005.wav",
+    "wmd": "demo/librispeech/ours/librispeech_7127-75947-0005_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_7176-92135-0038.wav",
+    "wmd": "demo/librispeech/ours/librispeech_7176-92135-0038_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_8230-279154-0016.wav",
+    "wmd": "demo/librispeech/ours/librispeech_8230-279154-0016_watermarked.wav",
+},
+{
+    // "dataset": "LibriSpeech",
+    "origin": "demo/librispeech/gt/librispeech_8230-279154-0029.wav",
+    "wmd": "demo/librispeech/ours/librispeech_8230-279154-0029_watermarked.wav",
+},
+
+//  dataset2
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ005-0291.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ005-0291_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ008-0091.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ008-0091_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ011-0172.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ011-0172_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ018-0111.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ018-0111_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ028-0304.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ028-0304_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ033-0135.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ033-0135_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ037-0168.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ037-0168_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ039-0233.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ039-0233_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ044-0067.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ044-0067_watermarked.wav",
+},
+{
+    "origin": "demo/ljspeech/gt/ljspeech_LJ049-0117.wav",
+    "wmd": "demo/ljspeech/ours/ljspeech_LJ049-0117_watermarked.wav",
+},
+
+
+]
